3bf0dbab483965763c85b32aed2935784a39d1bc
AbdinurH-data <husseinabdinur13@gmail.com>
2026-04-22 14:14:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python revision:
+  -- to run a python script `python filename.py`
+## Quick keys:
+  -- open and close terminal `ctrl + ~`
+  -- clearing or cleaning the terminal `ctrl + l`
+  -- to view previous terminal commands cycle `up - arrow` or `down-arrow`
+  -- to taggle a comment `ctrl + ?`
+
+  ## Build-it functions:
+
+    -- `print`
---
+++ welcome.py
@@ -0,0 +1,34 @@
+# Single line comments
+print("Welcome to python!!!")
+
+'''
+    for multi line comments 
+'''
+
+print("Per Scholas is Unlocking Potential and changing the Face of tech")
+
+print(10+98)
+
+print(300.5 + 0.5)
+
+# sep= attribute/parameter puts a separator of choice between the comma
+# separated value
+print(12, 24, -2, sep=';')
+
+print('but', 'not', 'including', sep='**')
+
+
+print('but', 'not', 'including', sep='')
+
+# saving response into variable
+
+name = input('What is your name?: ')
+print('Hello', name, '\n what is up?')
+
+# Get numbers from user 
+
+nb = input('Enter your mobile number: ')
+hp = input('Enter your Home number: ')
+
+print('Mobile number is ', nb)
+print('Home number is ', hp)
